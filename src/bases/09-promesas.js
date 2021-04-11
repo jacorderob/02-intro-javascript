dfc53ad7ab4fa5f116cc8e644f8df4c6fda1d5a5
Andrés Cordero <jrcorder@uc.cl>
2021-04-11 14:32:31 -0400

    Class 9: promises

---
+++ src/bases/09-promesas.js
@@ -0,0 +1,29 @@
+import { getHeroeById } from './bases/08-imp-exp';
+
+// Promises
+/* const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const heroe = getHeroeById(2);
+    resolve(heroe);
+    // reject('No heroe found')
+  }, 2000);
+});
+
+promise.then((heroe) => {
+  console.log(heroe)
+})
+.catch(err => console.warn(err)); */
+
+const getHeroeByIdAsync = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const heroe = getHeroeById(id);
+      if (heroe) resolve(heroe);
+      else reject('No heroe available for you...')
+    }, 2000);
+  });
+}
+
+getHeroeByIdAsync(40)
+  .then(console.log)
+  .catch(console.warn)
